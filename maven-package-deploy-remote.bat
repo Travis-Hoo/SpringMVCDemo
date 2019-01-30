677dda234cc02f577bbab875fe22598582e079fb
Travis <806616097@qq.com>
2019-01-30 17:42:39 +0800

    update pom.xml

--- maven-package-deploy-remote.bat
+++ maven-package-deploy-remote.bat
@@ -2,8 +2,7 @@
 echo 自动部署tomcat开始...
 echo 当前时间是：%DATE% %time%
 call mvn clean package
-cd target/ssmDemo
-call mvn tomcat8:redeploy -Dmaven.test.skip=true
+call mvn tomcat7:redeploy -Dmaven.test.skip=true
 echo 部署完成，当前时间是：%DATE% %time%
 echo 服务器启动于 %DATE% %time% ，successful completed！
 pause
